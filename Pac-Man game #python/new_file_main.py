9b42451b4d531e3fbb6b73e83f0521e20c21ec38
Stan Sabin <122362101+SabinGhost19@users.noreply.github.com>
2024-03-02 00:04:29 +0200

    Add files via upload

---
+++ Pac-Man game #python/new_file_main.py
@@ -0,0 +1,13 @@
+import copy
+from board import boards
+import pygame
+import math
+
+
+pygame.init()
+
+Inaltimea=800
+Lungimea=800
+screen=pygame.display.set_mode([Inaltimea,Lungimea])
+
+pygame.quit()
